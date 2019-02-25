9d28502c775de480669b58d5c43178cbdf013186
cilegann <s571991@gmail.com>
2019-02-25 16:47:52 +0800

    balanced data, unfinished

--- src/capsulenet.py
+++ src/capsulenet.py
@@ -15,6 +15,7 @@ from keras.optimizers import Adam
 from Capsule_Keras import *
 from evaluate_tools import plot_confusion_matrix,evaluate
 import keras.backend.tensorflow_backend as KTF
+from utils import *
 
 width=420
 height=131
@@ -22,6 +23,7 @@ num_of_classes=3
 batch_size=32
 train_mapping_file='./data/CNN_x_y_mapping.csv'
 vali_mapping_file='./data/CNN_vali_x_y_mapping.csv'
+mappings=[train_mapping_file,vali_mapping_file]
 
 polluted_train_basedir='./data/polluted'
 positive_train_basedir='./data/positive'
@@ -29,6 +31,8 @@ negative_train_basedir='./data/negative'
 polluted_vali_basedir='./data/vali/polluted'
 positive_vali_basedir='./data/vali/positive'
 negative_vali_basedir='./data/vali/negative'
+basedirs=[polluted_train_basedir,positive_train_basedir,negative_train_basedir,polluted_vali_basedir,positive_vali_basedir,negative_vali_basedir]
+
 
 def config_environment(args):
     global batch_size
@@ -38,101 +42,6 @@ def config_environment(args):
     KTF.set_session(session)
     batch_size=args.batch
     os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-###################################################################################
-
-def create_x_y_mapping(train_or_vali):
-    basedir_list=[]
-    if(train_or_vali=='train'):
-        mapping_file=train_mapping_file
-        basedir_list=[negative_train_basedir,positive_train_basedir,polluted_train_basedir]
-    else:
-        mapping_file=vali_mapping_file
-        basedir_list=[negative_vali_basedir,positive_vali_basedir,polluted_vali_basedir]
-    with open(mapping_file,'w') as f:
-        f.write("file_path,label\n")
-        for i,b in enumerate(basedir_list):
-            for root, directs,filenames in os.walk(b):
-                for filename in filenames:
-                    if 'txt' not in filename:
-                        pathName=os.path.join(root,filename)
-                        if( ('jpg' in pathName) or ('png' in pathName) ):
-                            f.write(pathName+','+str(i)+'\n')
-
-###################################################################################
-
-def read_x_y_mapping(train_or_vali,shuffle):
-    file_list=[]
-    y=[]
-    if(train_or_vali=='train'):
-        mapping_file=train_mapping_file
-    else:
-        mapping_file=vali_mapping_file
-    if(not os.path.exists(mapping_file)):
-        create_x_y_mapping(train_or_vali)
-    with open(mapping_file,'r') as f:
-        next(f)
-        lines=f.readlines()
-        for line in lines:
-            file_list.append(line.split(',')[0])
-            y.append(line.split(',')[1][:-1])
-    if(shuffle):
-        c=list(zip(file_list,y))
-        random.shuffle(c)
-        file_list,y=zip(*c)
-    return file_list,np_utils.to_categorical(np.array(y),num_of_classes)
-    
-###################################################################################
-
-def preprocessing_augment(data,label):
-    data=data.resize([width,height])
-    data = np.asarray(data)
-    data = data.astype('float64')
-    if (random.random() > 0.5 and int(label[1])==1):
-        data = cv2.flip(data, 1)
-    data/=255.
-    return data
-
-###################################################################################
-
-train_index=0
-vali_index=0
-train_indexes
-def data_generator(is_training,file_lists,y,is_balanced):
-    if is_balanced:
-        pass
-    else:
-        global train_index
-        global vali_index
-        while(1):
-            if is_training == True:
-                if train_index + batch_size > len(file_lists):
-                    train_index = 0
-                train_index += batch_size
-                index=train_index
-            else:
-                if vali_index + batch_size > len(file_lists):
-                    vali_index = 0
-                vali_index += batch_size
-                index=vali_index
-        
-            file_list = file_lists[index-batch_size:index]
-            label_list = y[index-batch_size:index]
-            output = np.zeros([batch_size, height,width, 3])
-            for i in range(batch_size):
-                output[i]=preprocessing_augment(Image.open(file_list[i]),label_list[i])
-            yield output, label_list
-
-###################################################################################
-
-def load_all_valid(file_list):
-    x_vali=np.zeros([len(file_list),height,width,num_of_classes])
-    for i,f in enumerate(file_list):
-        x_vali[i]=Image.open(f).resize([width,height])
-    x_vali=x_vali.astype('float64')
-    x_vali/=255.
-    return x_vali
-
-###################################################################################
 
 def marginLoss(y_true,y_pred):
     return y_true*K.relu(0.9-y_pred)**2+0.5*(1-y_true)*K.relu(y_pred-0.1)**2
@@ -166,9 +75,9 @@ def train(args):
     cbckpt=ModelCheckpoint('./models/capsule_'+nowtime+'_best.h5',monitor='val_loss',save_best_only=True)
     cbes=EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
     model.compile(loss=marginLoss,optimizer=Adam(),metrics=['accuracy'])
-    x_train_list,y_train=read_x_y_mapping('train',True)
-    x_vali_list,y_vali=read_x_y_mapping('vali',False)
-    x_vali=load_all_valid(x_vali_list)
+    x_train_list,y_train=read_x_y_mapping(mappings,basedirs,'train',True,args)
+    x_vali_list,y_vali=read_x_y_mapping(mappings,basedirs,'train',True,args)
+    x_vali=load_all_valid(x_vali_list,args)
     model.fit_generator(data_generator(True,x_train_list,y_train),
                         validation_data=(x_vali,y_vali),
                         validation_steps=1,
@@ -188,8 +97,8 @@ def train(args):
 
 def test(args):
     model=load_model(args.model)
-    x_vali_list,y_vali=read_x_y_mapping('vali',False)
-    x_vali=load_all_valid(x_vali_list)
+    x_vali_list,y_vali=read_x_y_mapping(mappings,basedirs,'vali',False,args)
+    x_vali=load_all_valid(x_vali_list,args)
     y_pred=model.predict(x_vali)
     y_pred=np.argmax(y_pred,axis=1)
     y_true=np.argmax(y_vali,axis=1)
@@ -227,6 +136,10 @@ if __name__ == "__main__":
     parser.add_argument('-b','--batch',type=int,default=32,help='Batch size')
     parser.add_argument('-e','--epochs',type=int,default=200,help='#Epochs')
     parser.add_argument('-s','--share',action='store_true',help='Share weight or not')
+    parser.add_arguments('--width',type=int,default=420)
+    parser.add_arguments('--height',type=int,default=131)
+    parser.add_arguments('--balance',action='store_true',help="Balance data by undersampling the majiroty data")
+    parser.add_arguments('--n_labels',type=int,default=3)
     args=parser.parse_args()
     print(args)
     config_environment(args)
