bdf4dc1e65cba0817073d1aa9f06724cf1da0898
cilegann <s571991@gmail.com>
2019-03-15 14:30:04 +0800

    test

--- src/two_stage_cnn.py
+++ src/two_stage_cnn.py
@@ -30,167 +30,27 @@ import platform
 from sklearn.metrics import confusion_matrix
 import scipy.ndimage as ndimage
 from evaluate_tools import plot_confusion_matrix,evaluate
-height=131
-width=420
-epoch=200
-vali_split=0.3
-
-host=platform.node()
-mode=os.sys.argv[1]
-if(host=='ican-1080ti'and mode=='train' and ('-both' in os.sys.argv)):
-    gpu='both'
-else:
-    gpu='single'
-
-if(gpu=='single'):
-    os.environ['CUDA_VISIBLE_DEVICES']='1'
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth=True
-    session = tf.Session(config=config)
-    KTF.set_session(session)
-    batch_size=32
+from utils import *
 
 train_mapping_file='./data/CNN_x_y_mapping.csv'
 vali_mapping_file='./data/CNN_vali_x_y_mapping.csv'
+mappings=[train_mapping_file,vali_mapping_file]
 
-# if (host=='cilegann-PC'):
-#     polluted_train_basedir='./original_data/categ/polluted'
-#     positive_train_basedir='./original_data/categ/positive'
-#     negative_train_basedir='./original_data/categ/negative'
-#     polluted_vali_basedir='./data/x'
-#     positive_vali_basedir='./data/p'
-#     negative_vali_basedir='./data/n'
-# if (host=='ican-1080ti'):
 polluted_train_basedir='./data/polluted'
 positive_train_basedir='./data/positive'
 negative_train_basedir='./data/negative'
 polluted_vali_basedir='./data/vali/polluted'
 positive_vali_basedir='./data/vali/positive'
 negative_vali_basedir='./data/vali/negative'
+basedirs=[polluted_train_basedir,positive_train_basedir,negative_train_basedir,polluted_vali_basedir,positive_vali_basedir,negative_vali_basedir]
 
-if(host=='cilegann-PC' or gpu=='single'):
-    batch_size=32
-else:
-    batch_size=64
-
-index = 0
-vali_index = 0
-
-train_x_file_list = []
-train_x_file_list_backup=[]
-train_x = []
-train_y = []
-train_y_backup=[]
-num_of_0=0
-num_of_1=0
-
-vali_x_file_list = []
-vali_x_file_list_backup=[]
-vali_x=[]
-vali_y = []
-vali_y_backup=[]
-
-###################################################################################
-
-def create_x_y_mapping(train_or_vali):
-    basedir_list=[]
-    if(train_or_vali=='train'):
-        mapping_file=train_mapping_file
-        basedir_list=[negative_train_basedir,positive_train_basedir,polluted_train_basedir]
-    else:
-        mapping_file=vali_mapping_file
-        basedir_list=[negative_vali_basedir,positive_vali_basedir,polluted_vali_basedir]
-    with open(mapping_file,'w') as f:
-        f.write("file_path,label\n")
-        for i,b in enumerate(basedir_list):
-            for root, directs,filenames in os.walk(b):
-                for filename in filenames:
-                    pathName=os.path.join(root,filename)
-                    if( ('jpg' in pathName) or ('png' in pathName) ):
-                        f.write(pathName+','+str(i)+'\n')
-
-###################################################################################
-
-def read_x_y_mapping(train_or_vali,shuffle):
-    if(train_or_vali=='train'):
-        global train_x_file_list_backup
-        global train_y_backup
-        file_list=[]
-        y=[]
-        mapping_file=train_mapping_file
-    else:
-        global vali_x_file_list_backup
-        global vali_y_backup
-        file_list=[]
-        y=[]
-        mapping_file=vali_mapping_file
-    if(not os.path.exists(mapping_file)):
-        create_x_y_mapping(train_or_vali)
-    with open(mapping_file,'r') as f:
-        next(f)
-        lines=f.readlines()
-        for line in lines:
-            file_list.append(line.split(',')[0])
-            y.append(line.split(',')[1][:-1])
-    if(shuffle):
-        c=list(zip(file_list,y))
-        random.shuffle(c)
-        file_list,y=zip(*c)
-    if(train_or_vali=='train'):
-        train_x_file_list_backup=file_list
-        train_y_backup=np_utils.to_categorical(np.asarray(y),3)
-    else:
-        vali_x_file_list_backup=file_list
-        vali_y_backup=np_utils.to_categorical(np.asarray(y),3)
-
-###################################################################################
-
-def load_all_valid(vali_x_file_list):
-    x = np.zeros([len(vali_x_file_list), height, width, 3])
-    for i,f in enumerate(vali_x_file_list):
-        x[i]=Image.open(f).resize([width,height])
-    x=x.astype('float64')
-    x/=255.
-    return x
-    #TODO backup
-
-###################################################################################
-
-def resize_preprocessing(data,label):
-    data=data.resize([width,height])
-    data = np.asarray(data)
-    data = data.astype('float64')
-    if (random.random() > 0.5 and int(label[1])==1):
-        data = cv2.flip(data, 1)
-    data/=255.
-    return data
-
-###################################################################################
-
-def data_generator(is_training):
-    global index
-    global vali_index
-    while(1):
-        if is_training == True:
-            if index + batch_size > len(train_x_file_list):
-                index = 0
-            file_list = train_x_file_list[index:index + batch_size]
-            label_list = train_y[index:index + batch_size]
-            index += batch_size
-        else:
-            if vali_index + batch_size > len(vali_x_file_list):
-                vali_index = 0
-            file_list = vali_x_file_list[vali_index:vali_index + batch_size]
-            label_list = vali_y[vali_index:vali_index + batch_size]
-            vali_index += batch_size
-
-        output = np.zeros([batch_size, height,width, 3])
-        for i in range(batch_size):
-            output[i]=resize_preprocessing(Image.open(file_list[i]),label_list[i])
-
-        yield output, label_list
-
-###################################################################################
+def config_environment(args):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth=True
+    session = tf.Session(config=config)
+    KTF.set_session(session)
+    batch_size=args.batch
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 def get_model():
     model = Sequential()
