84c14f1293916eccd17e8fc84581cc3f7c3e14d7
cilegann <s571991@gmail.com>
2019-07-15 13:31:59 +0800

    new models

--- saved_do_not_del_src/transfer_vgg_2019-07-14-21:24.py
+++ saved_do_not_del_src/transfer_vgg_2019-07-14-21:24.py
@@ -34,6 +34,8 @@ def config_environment(args):
 
 def get_model(args):
     model = VGG16(weights='imagenet', include_top=True)
+    for l in model.layers:
+        l.trainable=False
     model.layers.pop()
     model.layers.pop()
     hidden=Dropout(0.5)(model.layers[-2].output)
@@ -41,7 +43,10 @@ def get_model(args):
     hidden=BatchNormalization()(hidden)
     x=Dense(args.n_labels, activation='softmax')(hidden)
     model=Model(model.input,x)
+
     model.summary()
+    for l in model.layers:
+        print(l,l.trainable)
     return model
 
 ###################################################################################
